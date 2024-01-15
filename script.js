75d244a9994506620cf936934a0e5b5258fae9f4
Jia Gapuz <jiannagapuz@gmail.com>
2024-01-14 17:06:03 -0800

    js practice

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 let x = 1;
 console.log(x);
+const mainHeading = document.getElementById("main_heading");
+mainHeading.innerHTML = "Anotherer heading";
+mainHeading.style = "color: red";
+
+
+//---------------
+
+//define my object
+let myObject = {
+    myField: 12,
+    myOtherField: 13
+};
+
+//define myVariable
+let myVariable = true;
+
+//logs both to console
+console.log(myObject);
+console.log(typeof myVariable);
+
+//PUT A PIN ON THIS
+const myFunction = (x) => x * x;
+console.log(myFunction);
+
+
